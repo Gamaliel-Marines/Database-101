a1c03a9330240b8f278730be969fe3bdfd3408d1
Gamaliel-Marines <gamaliel_projects@hotmail.com>
2024-02-02 20:50:06 -0600

    Feat: create tables

---
+++ Queries/queries1.sql
@@ -0,0 +1,8 @@
+CREATE TABLE athletes (
+    athlete_id INT PRIMARY KEY,
+    first_name varchar(50),
+    last_name varchar(50),
+    best_event varchar(50),
+    best_time decimal(10,2),
+    trails_time decimal(10,2)
+);
